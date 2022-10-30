de3b9f0cbff036770c0bb70febee88c9db3e8395
Anasemos Kassahun <anasemosaka@gmail.com>
2022-10-29 23:43:29 -0400

    wip

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './Gallery.css';
+import "./Gallery.css";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => <div></div>;
 
 export default Gallery;
