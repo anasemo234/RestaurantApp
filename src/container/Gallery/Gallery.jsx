c4deca50c18c4ea7f9ca307bc4f76ec30a815305
Anasemos Kassahun <anasemosaka@gmail.com>
2022-10-30 22:09:35 -0400

    wip

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 import "./Gallery.css";
 
 const Gallery = () => <div></div>;
